fec35292ec612e4fa5297b4011b54265ec822a07
etboi1 <et.grundvig@gmail.com>
2026-02-13 16:29:11 -0700

    Added most of the validation needed for AddMovie.cshtml

--- Mission06_Grundvig/Models/Movie.cs
+++ Mission06_Grundvig/Models/Movie.cs
@@ -1,15 +1,22 @@
-﻿namespace Mission06_Grundvig.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Grundvig.Models
 {
     public class Movie
     {
         public int MovieID { get; set; }
-        public Category Category { get; set; }
+        [Required]
+        public Category? Category { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int Year { get; set; }
+        [Required]
         public string Director { get; set; }
-        public Rating Rating { get; set; }
-        public bool Edited { get; set; }
-        public string LentTo { get; set; }
-        public string Notes { get; set; }
+        [Required]
+        public Rating? Rating { get; set; }
+        public bool? Edited { get; set; }
+        public string? LentTo { get; set; }
+        public string? Notes { get; set; }
     }
 }
