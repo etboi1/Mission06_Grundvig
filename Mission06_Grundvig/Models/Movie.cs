15a3ae4e7ef9afe04cdfef56a06ccfb57fcdc079
etboi1 <et.grundvig@gmail.com>
2026-02-13 16:34:28 -0700

    Added the final validation for the AddMovie.cshtml (limiting notes to 25 characters)

--- Mission06_Grundvig/Models/Movie.cs
+++ Mission06_Grundvig/Models/Movie.cs
@@ -17,6 +17,7 @@ namespace Mission06_Grundvig.Models
         public Rating? Rating { get; set; }
         public bool? Edited { get; set; }
         public string? LentTo { get; set; }
+        [StringLength(25, ErrorMessage = "Notes cannot exceed 25 characters.")]
         public string? Notes { get; set; }
     }
 }
