8c3a02091aaf6e5a00af464d923670bcf075d984
etboi1 <et.grundvig@gmail.com>
2026-02-12 20:56:40 -0700

    Created the form for adding a movie. Also needed to change the Enums to display properly.
    TODO:
    - Fix Notes so that it can only take 25 characters
    - Style form correctly with bootstrap
    - Style form button correctly with bootstrap

--- Mission06_Grundvig/Models/Category.cs
+++ Mission06_Grundvig/Models/Category.cs
@@ -1,11 +1,15 @@
-﻿namespace Mission06_Grundvig.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Grundvig.Models
 {
     public enum Category
     {
+        [Display(Name = "Action/Adventure")]
         ActionAdventure,
         Comedy,
         Drama,
         Family,
+        [Display(Name = "Horror/Suspense")]
         HorrorSuspense,
         Miscellaneous,
         Telivision,
--- Mission06_Grundvig/Models/Rating.cs
+++ Mission06_Grundvig/Models/Rating.cs
@@ -1,9 +1,12 @@
-﻿namespace Mission06_Grundvig.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Grundvig.Models
 {
     public enum Rating
     {
         G,
         PG,
+        [Display(Name = "PG-13")]
         PG13,
         R
     }
