64795b5cd6a0d2836120441df3303f2f0385ec92
etboi1 <et.grundvig@gmail.com>
2026-02-13 18:22:47 -0700

    Made change to the database

---
+++ Mission06_Grundvig/Migrations/20260214011944_YearRangeValidation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission06_Grundvig.Migrations
+{
+    /// <inheritdoc />
+    public partial class YearRangeValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
